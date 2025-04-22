644083897cdf25214b0986f51c336bc4b37352b6
Natiele Nogueira de Paula <natiele887@gmail.com>
2025-04-22 20:36:27 -0300

    Elementos de gastos

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 // Elementos de Tarefas
 const inputTarefa = document.getElementById('novaTarefa');
 const listaTarefas = document.getElementById('listaDeTarefas');
+
+// Elementos de Gastos
+const inputDescricaoGasto = document.getElementById('descricaoGasto');
+const inputValorGasto = document.getElementById('valorGasto');
+const listaGastos = document.getElementById('listaDeGastos');
+const totalGastoElemento = document.getElementById('totalGasto');
+let totalGastos = 0;
